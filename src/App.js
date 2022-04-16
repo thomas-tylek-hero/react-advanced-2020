aafb1128ec2a11125c2599733697c0132504a79c
Thomas Tylek <thomas.tylek@herodigital.com>
2022-04-16 17:07:02 -0400

    intro set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Setup from './tutorial/1-useState/setup/2-useState-basics';
+// import Final from './tutorial/1-useState/final/2-useState-basics';
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <Setup />
+      {/* <Final /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
