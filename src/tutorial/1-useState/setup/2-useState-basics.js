109ea256b41247b7ad4e5f688feca5918f45c22c
Thomas Tylek <thomas.tylek@herodigital.com>
2022-04-16 17:07:23 -0400

    use state example with random word fetch

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
+// useState - function
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello world'));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+  const [text, setText] = useState('Random Title');
+
+  const handleClick = () => {
+    fetch('https://random-word-api.herokuapp.com/word')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return response.json();
+      })
+      .then((response) => {
+        setText(response[0]);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <>
+      <h2>{text}</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+        Change Title
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
