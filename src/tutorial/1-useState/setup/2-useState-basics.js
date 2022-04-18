c571691360f6f40d3fa042960dfa6965f9b9274a
Thomas Tylek <thomas.tylek@herodigital.com>
2022-04-18 12:05:13 -0400

    generate random word on interval, animate interval

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,45 +1,99 @@
 import React, { useState, useEffect } from 'react';
+import { useSprings, config, animated } from 'react-spring';
 // useState - function
 
 const UseStateBasics = (props) => {
-  const [wordCache, setWordCache] = useState(['Random Title']);
+  // const [words, setWords] = useState(['random', 'words', 'generator']);
+  // const [wordCache, setWordCache] = useState(['tacit']);
 
-  const fetchRandomWordBatch = async (size) => {
-    return fetch(`https://random-word-api.herokuapp.com/word/?number=${size}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .catch((error) => console.log(error));
-  };
+  // const fetchRandomWordBatch = async (size) => {
+  //   return fetch(`https://random-word-api.herokuapp.com/word/?number=${size}`)
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! Status: ${response.status}`);
+  //       }
+  //       return response.json();
+  //     })
+  //     .catch((error) => console.log(error));
+  // };
 
-  useEffect(() => {
-    console.log(setWordCache);
-    fetchRandomWordBatch(20).then((wordArr) => {
-      setWordCache([...wordCache, ...wordArr]);
-    });
-  }, []);
+  // const fillWords = () => {
+  //   fetchRandomWordBatch(30)
+  //     .then((wordArr) => {
+  //       setWordCache([...wordArr]);
+  //     })
+  //     .then(() => {
+  //       setWords([...words, ...wordCache]);
+  //     });
+  // };
 
-  useEffect(() => {
-    if (wordCache.length < 6) {
-      fetchRandomWordBatch(20).then((wordArr) => {
-        setWordCache([...wordCache, ...wordArr]);
-      });
-    }
-  }, [wordCache]);
+  // useEffect(() => {
+  //   setWords(words.slice(1));
+  // };
+  //   if (words.length < 6) {
+  //     fillWords();
+  //   }
+  // }, [words]);
 
-  const handleClick = () => {
-    setWordCache(wordCache.slice(1));
-  };
+  // const handleClick = () => {
+
+  const [wordsCache, setWordsCache] = useState([
+    'random',
+    'words',
+    'generator',
+    'tacit',
+    'leakproof',
+    'intercooler',
+    'belched',
+    'uprousing',
+    'plectra',
+    'overcomers',
+    'clotted',
+    'sisals',
+    'repartees',
+    'spinals',
+    'alp',
+    'unmanning',
+    'modernisation',
+    'ribozyme',
+    'titanesses',
+    'feverworts',
+    'lotting',
+    'unlevels',
+    'offcast',
+    'centigrams',
+    'owns',
+    'understatedly',
+    'acromion',
+    'ribbers',
+    'synching',
+    'letching',
+    'lulls',
+    'usualness',
+    'ptomainic',
+    'oncoming',
+  ]);
+
+  const [index] = useSprings({
+    from: { val: 0 },d
+    to: { val: wordsCache.length - 1 },
+    delay: 200,
+    config: config.default,
+  });
 
   return (
     <>
-      <h2>{wordCache[0]}</h2>
-      <button type='button' className='btn' onClick={handleClick}>
-        Change Title
-      </button>
+      <h2>
+        <animated.div>
+          {/* {wordsCache[parseInt(index.val.to((val) => Math.floor(val)))]} */}
+          {/* {index.val.to((val) => Math.floor(val))} */}
+        </animated.div>
+      </h2>
+      {/* <button type='button' className='btn' onClick={handleClick}>
+        <span>{`Turn Generator ${status}`}</span>
+        <span className='generator-toggle'></span>
+      </button> */}
+      <input type='range' />
     </>
   );
 };
